b067de9002e4acc4130b1bdb1a4fd6a99d4a0f50
Cody Condon <codycondon50@gmail.com>
2016-01-25 17:21:28 -0500

    Making images show at /images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Image.create!(
+  :title  => "The old church on the coast of White sea",
+  :artist => "Sergey Ershov",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+)
+
+Image.create!(
+  :title  => "Sea Power",
+  :artist => "Stephen Scullion",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+)
+
+Image.create!(
+  :title  => "Into the Poppies",
+  :artist => "John Wilhelm",
+  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+)
